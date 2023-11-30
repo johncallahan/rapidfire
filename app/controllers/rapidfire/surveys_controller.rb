0cf5ee8d5270856b077696d145a657d7b516b618
John Callahan <jcallahan@acm.org>
2023-11-29 21:25:26 -0500

    add active boolean as string parameter

--- app/controllers/rapidfire/surveys_controller.rb
+++ app/controllers/rapidfire/surveys_controller.rb
@@ -73,7 +73,7 @@ module Rapidfire
     private
 
     def survey_params
-      params.require(:survey).permit(:name, :introduction, :after_survey_content)
+      params.require(:survey).permit(:name, :introduction, :after_survey_content, :active)
     end
   end
 end
